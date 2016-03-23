9820b0e3a7d0c8b517d91fba1ea03906f512aac3
tanuja <root@githost.(none)>
2016-03-23 08:47:55 +0000

    latest upadate

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
  <body bgcolor="#FFF66C">
   <h1> Version 2.0 <h1>
-  <h2> Sample PHP App <h2>
+  <h2> Sample PHP Application <h2>
   <h2> Deployed with Capistrano <h2>
     <?php
     echo "<h4>Current date and time => " . date("r") . "</h4>";
